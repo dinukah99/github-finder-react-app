9a588c2e7bf1ee7d9a6cb9fcdde76d85802af8e7
dinukah99 <dinukahamarasinghe@gmail.com>
2024-01-29 20:03:26 +0530

    adding github token

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-
 function Home(props) {
     return (
-        <h1 className="text-6xl">Welcome</h1>
+        <div>
+            <h1 className="text-6xl">Welcome</h1>
+        </div>
     );
 }
 
